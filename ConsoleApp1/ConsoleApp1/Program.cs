39ed66625b251adeb855e2b1fb38d9e4cada12ec
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-07 20:01:59 +0400

    new proekt

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(count("azad",'b'));
+            Console.ReadLine(); 
+        }
+        static int count(string ad,char herf)
+        {
+            int a = 0;
+            for (int i = 0; i < ad.Length; i++)
+            {
+                if (ad[i] == herf)
+                {
+                    a++;
+                }
+            }
+            return a;
+           
+        }
+    }
+}
